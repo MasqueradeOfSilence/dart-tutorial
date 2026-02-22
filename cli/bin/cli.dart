121a08d1aca1cb9cf26c3f65b90ae1718f24220c
MasqueradeOfSilence <alexcn711@gmail.com>
2026-02-21 20:30:32 -0700

    Search begun

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,13 +1,21 @@
 const version = '0.0.1';
 void main(List<String> arguments) 
 {
-  if (arguments.isEmpty)
+  if (arguments.isEmpty || arguments.first == 'help')
   {
-    print('Hello, Dart!');
+    printUsage();
   }
   else if (arguments.first == 'version')
   {
     print('Dartpedia CLI version $version');
+  }
+  else if (arguments.first == 'search')
+  {
+    print('Search command recognized!');
+  }
+  else
+  {
+    printUsage();
   }
 }
 
