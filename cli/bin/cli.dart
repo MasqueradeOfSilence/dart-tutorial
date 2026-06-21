f153d66cce316c32bc69f732b97112e2e7e6b84c
MasqueradeOfSilence <alexcn711@gmail.com>
2026-06-20 21:37:17 -0600

    step 6

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -73,4 +73,4 @@ Future<String> getWikipediaArticle(String articleTitle) async
   return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
 // next step: https://dart.dev/learn/tutorial/async
-// Task 5: Update main to call searchWikipedia
+// Task 6: run the application
